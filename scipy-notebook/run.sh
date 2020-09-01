ae10517cd568a9774c5e46d8d879592d6285134e
jl2035 <jaka.luthar@gmail.com>
2020-09-01 16:37:30 +0200

    fix

--- scipy-notebook/run.sh
+++ scipy-notebook/run.sh
@@ -3,5 +3,6 @@
 docker stop jupitery
 docker rm jupitery
 HOMEDIR="$(pwd)/home_dir"
+docker build -t jupitery_scipy_notebook:1.0.0 .
 docker run -d --name jupitery -e JUPYTER_ENABLE_LAB=yes -p 8888:8888 -v $HOMEDIR:/home/jovyan jupitery_scipy_notebook:1.0.0
 
